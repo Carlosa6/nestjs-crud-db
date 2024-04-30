54dc1c37a59999f2272e7604f37999b13fe7eb25
Aaron Carrillo <k5r6ri89yo@gmail.com>
2024-04-29 19:23:59 -0500

    feat: Interaacion entre modulos

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { UsersService } from '../services/users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+  constructor(private userService: UsersService) {}
+
+  @Get()
+  findAll(){
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  getUser(@Param('id',ParseIntPipe) id: number){
+    return this.userService.findOne(id)
+  }
+
+  @Get(':id/orders')
+  getOrdersByUser(@Param('id',ParseIntPipe) id: number){
+    return this.userService.getOrdersByUser(id)
+  }
+
+}
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { User } from '../entities/user.entity';
+import { Order } from '../entities/order.entity';
+import { ProductsService } from 'src/products/services/products.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private productService: ProductsService) {}
+
+  private counterId = 1;
+  private users: User[] = [
+    {
+      id: 1,
+      name: 'jose peter',
+      email: 'jose73@gmail.com',
+    },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    let product = this.users.find((item) => item.id === id);
+
+    if (!product) {
+      throw new NotFoundException(`User #${id} not found.`);
+    }
+
+    return product;
+  }
+
+  create(payload: any) {
+    this.counterId = this.counterId + 1;
+    const newUser = {
+      id: this.counterId,
+      ...payload,
+    };
+
+    this.users.push(newUser);
+
+    return newUser;
+  }
+
+  update(id: number, payload: any) {
+    const user = this.findOne(id);
+
+    const index = this.users.findIndex((item) => item.id === id);
+    this.users[index] = { ...user, ...payload };
+    return this.users[index];
+  }
+
+  remove(id: number) {
+    const index = this.users.findIndex((item) => item.id === id);
+    this.users.splice(index, 1);
+    return true;
+  }
+
+  getOrdersByUser(id: number): Order {
+    const user = this.findOne(id);
+    return {
+      date: new Date(),
+      user,
+      products: this.productService.findAll(),
+    };
+  }
+}
